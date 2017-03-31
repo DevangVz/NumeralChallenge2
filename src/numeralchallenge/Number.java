4e55c4c392ba93184229ce86585f7fa0be1f9dbd
DevangVz <devang.vazquez@gmail.com>
2017-03-31 04:29:59 -0600

    githubFirstS

--- src/numeralchallenge/Number.java
+++ src/numeralchallenge/Number.java
@@ -5,23 +5,27 @@
  */
 package numeralchallenge;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Devang
  */
 public class Number {
-    String tupla;
-    
+    String string;
+    int limit;
+    LinkedList<Integer> values;
     public Number(String tupla) throws ExceptionString{
-         if(!tupla.matches("^[([0-9])+(,)([0-9])+]")){
+         if(!tupla.matches("^\\[([0-9])+(,)([0-9])+\\]")){
             throw new ExceptionString("Are you even trying? Syntax [<<number>>,<<number>>] ");
         }
-        String[] alfa= tupla.substring(1, tupla.length()-2).split(",");
-        String numero= alfa[0];
-        int length= Integer.parseInt(alfa[1]);
-        if(length>=alfa.length-1)
+        String[] alfa= tupla.substring(1, tupla.length()-1).split(",");
+        string= alfa[0];
+        limit= Integer.parseInt(alfa[1]);
+        if(limit>=string.length()&&limit>1)
             throw new ExceptionString("Yeaaaaah.... nope... Length for 2nd entry doesn't fit");
-        
     }
+    public void setValues(){}
+    
     
 }
--- src/numeralchallenge/NumeralChallenge.java
+++ src/numeralchallenge/NumeralChallenge.java
@@ -5,6 +5,9 @@
  */
 package numeralchallenge;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Devang
@@ -15,9 +18,13 @@ public class NumeralChallenge {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
+        try {
+            // TODO code application logic here
+
+            Number n = new Number("[344,3]");
+        } catch (ExceptionString ex) {
+            System.err.println(ex);
+        }
     }
     
 }
